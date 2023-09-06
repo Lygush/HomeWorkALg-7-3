53f94d75dbba38c2b537d63f8c0e2900f3b3b407
Ivan <kosarevsky2000@yandex.ru>
2023-09-06 20:44:58 +0300

    initial commit

---
+++ HomeWorkAlg-7-3.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+bool serch(std::vector<int>& vector, int numb) {
+    for (int i{}; i < vector.size(); ++i) {
+        if (vector[i] == numb) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool dfs(int** matrix, int i, int size, std::vector<int>& result, int& prev) {
+    for (int j{}; j < size; ++j) {
+        if (matrix[i][j] != 0) {
+            if (serch(result, j)) {
+                if (prev != j) {
+                    return true;
+                }
+            }
+            else {
+                prev = j;
+                if (result.size() == size - 1) {
+                    return false;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    std::fstream f_in("input.txt");
+    int size{};
+    if (f_in.is_open()) {
+        f_in >> size;
+        int** matrix = new int*[size];
+        for (int i{}; i < size; ++i) {
+            matrix[i] = new int[size];
+        }
+        for (int i{}; i < size; ++i) {
+            for (int j{}; j < size; ++j) {
+                f_in >> matrix[i][j];
+            }
+        }
+        std::vector<int> result {0};
+        bool cycle{};
+        int prev{};
+        for (int i{}; i < size; ++i) {
+            for (int j{}; j < size; ++j) {
+               if (matrix[i][j] != 0) {
+                cycle = dfs(matrix, i, size, result, prev);
+               }
+            }
+        }
+        std::cout << ((cycle) ? "Cycle!" : "No cycle!");
+        for (int i{}; i < size; ++i) {
+            delete[] matrix[i];
+        }
+        delete[] matrix;
+    }
+    f_in.close();
+}
